3dfed761d541411355537e270d0dcbc8f7d5ac78
Markus Miller <39503241+MMillerCode12@users.noreply.github.com>
2025-04-28 09:09:16 -0400

    Update 20-Valid-Parentheses.py

--- 20-Valid-Parentheses.py
+++ 20-Valid-Parentheses.py
@@ -4,19 +4,13 @@ class Solution:
         parentheses_stack = []
         close_dict = {')': '(', ']': '[', '}': '{'}
 
-        for i in s:
-            if i == "(" or i == "[" or i =="{":
-                parentheses_stack.append(i)
-            elif i == ")" or i == "]" or i == "}":
-                if len(parentheses_stack) > 0:
-                    if close_dict[i] == parentheses_stack[-1]:
-                        parentheses_stack.pop()
-                    else:
-                        return False
+        for i in s: 
+            if i in close_dict:
+                if parentheses_stack and close_dict[i] == parentheses_stack[-1]:
+                    parentheses_stack.pop()
                 else:
                     return False
-        
-        if len(parentheses_stack) > 0:
-            return False
+            else:
+                parentheses_stack.append(i)
 
-        return True
+        return not parentheses_stack
